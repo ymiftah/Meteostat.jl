dac8daba30ae91fec3ea84f58fe5da12b149f3f1
Youssef Miftah <miftahyo@outlook.fr>
2025-04-21 17:06:34 +1000

    pages docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,14 @@
-using Documenter, Meteostat
+using Documenter, Meteostat, Literate
 
-makedocs(sitename="Meteostat.jl", remotes = nothing )
+makedocs(
+    sitename="Meteostat.jl",
+    # modules = [Meteostat],
+    remotes = nothing,
+    authors = "Youssef Miftah",
+    pages=[
+        "Welcome Page" => "index.md",
+        "Examples" => [
+            "Query hourly weather data by geo coordinates" => "examples/point_data.md",
+        ]
+    ]
+    )
