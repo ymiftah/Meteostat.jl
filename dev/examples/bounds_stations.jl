402f89c55fe71f9e5b36bd1c66facb5036c5d8fa
Documenter.jl <documenter@juliadocs.github.io>
2025-04-25 04:56:42 +0000

    build based on 8a4bbcd

---
+++ dev/examples/bounds_stations.jl
@@ -0,0 +1,22 @@
+"""
+Example: Get weather stations by geographical area
+
+Meteorological data provided by Meteostat (https://dev.meteostat.net)
+under the terms of the Creative Commons Attribution-NonCommercial
+4.0 International Public License.
+
+The code is licensed under the MIT license.
+"""
+
+using Meteostat
+
+# Get all stations
+stations = get_stations();
+
+# Get number of stations in northern hemisphere
+northern = filter_bounds!(copy(stations), (90., -180.), (0., 180.));
+@info "Stations in northern hemisphere: $(size(northern,1))"
+
+# Get number of stations in southern hemisphere
+southern = filter_bounds!(copy(stations), (0., -180.), (-90., 180.));
+@info "Stations in southern hemisphere:: $(size(southern,1))"
